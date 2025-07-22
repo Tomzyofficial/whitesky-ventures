b0b647f953494a153e26457cf395ef5a861a12ed
Tomzyofficial <tomzyofficial54@gmail.com>
2025-07-22 12:35:13 +0100

    Quick fix on connection.inc.php

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # This is an investment website with some limitations.
 
-## This website is only a learning project. I still own all the content and code, and you’re not allowed to reuse or copy it without asking me🚫.
+## This website is only a demo project. I still own all the content and code, and you’re not allowed to reuse or copy it without asking me🚫.
 
 -- Built with PHP, JavaScript, Bootstrap, and Vanilla CSS
--- app-dashboard/user/connection.inc.php
+++ app-dashboard/user/connection.inc.php
@@ -1,10 +1,10 @@
 <?php
 require_once("session.inc.php");
-/* $serverName = "localhost";
+$serverName = "localhost";
 $serverUser = "root";
 $serverPass = "";
 $dbname = "whitesky";
-$conn = mysqli_connect($serverName, $serverUser, $serverPass, $dbname); */
+$conn = mysqli_connect($serverName, $serverUser, $serverPass, $dbname);
 /* $serverName = "localhost";
 $serverUser = "whiteskyventures_ameliarosario";
 $serverPass = "chukwuebuka86";
@@ -14,16 +14,3 @@ $conn = mysqli_connect($serverName, $serverUser, $serverPass, $dbname); */
 $conn = new PDO($pdo, "whiteskyventures_ameliarosario", "chukwuebuka86"); */
 
 
-try {
-  $host =  $_ENV['DB_HOST'] ?? 'localhost';
-  $dbname = $_ENV['DB_NAME'] ?? 'whitesky';
-  $username = $_ENV['DB_USER'] ?? 'root';
-  $password = $_ENV['DB_PASSWORD'] ?? '';
-
-  $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-  // Use $pdo for database operations
-} catch(PDOException $e) {
-  die("Connection failed: " . $e->getMessage());
-}
